0cd6a7ec7d2e163f0c5d752ee929f9c2e78285a8
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-04-06 19:12:25 +0200

    clean benchmarks

--- src/wetree/Wetree.java
+++ src/wetree/Wetree.java
@@ -33,14 +33,12 @@ public class Wetree {
         boolean reset = true;
         wept.init(reset);
 
-//        buildFakeCorpus(wept, 5, 100);
-        
-//        benchmarkRandomWebEntity(wept, true);
-//        benchmarkAllWebEntities(wept, false);
+        buildFakeCorpus(wept, 1000, 1000000);
 
-//        foodchainBenchmark(wept);
+        List<WELink> links = wept.getWelinks();
+        System.out.println("Web Entity Links: " + links.size());
         
-        lruBenchmark(wept);
+//        lruBenchmark(wept);
         
 //        wept.log();
 
@@ -143,116 +141,6 @@ public class Wetree {
 //        wept.log();
     }
     
-    private static void foodchainBenchmark(WebEntityPageTree wept) throws IOException {
-        wept.addPage("Plankton:Phytoplankton");
-        wept.addPage("Plankton:Zooplankton");
-        wept.addPage("Crustacean:Prawn");
-        wept.addPage("Fish:Fish");
-        wept.addPage("Shell:Mussels");
-        wept.addPage("Crustacean:Crab");
-        wept.addPage("Bird:Gull");
-        wept.addPage("Plant:Seaweed");
-        wept.addPage("Shell:Limpets");
-        wept.addPage("Shell:Whelk");
-        wept.addPage("Crustacean:Lobster");
-        
-        WebEntities.getInstance().create("Plankton");
-        WebEntities.getInstance().create("Crustacean");
-        WebEntities.getInstance().create("Fish");
-        WebEntities.getInstance().create("Shell");
-        WebEntities.getInstance().create("Bird");
-        WebEntities.getInstance().create("Plant");
-        
-        ArrayList<PLink> pLinks = new ArrayList<>();
-        pLinks.add(new PLink("Plankton:Phytoplankton", "Plankton:Zooplankton"));
-        pLinks.add(new PLink("Plankton:Phytoplankton", "Shell:Mussels"));
-        pLinks.add(new PLink("Plankton:Zooplankton", "Crustacean:Prawn"));
-        pLinks.add(new PLink("Plankton:Zooplankton", "Shell:Mussels"));
-        pLinks.add(new PLink("Crustacean:Prawn", "Fish:Fish"));
-        pLinks.add(new PLink("Crustacean:Prawn", "Bird:Gull"));
-        pLinks.add(new PLink("Shell:Mussels", "Bird:Gull"));
-        pLinks.add(new PLink("Shell:Mussels", "Crustacean:Crab"));
-        pLinks.add(new PLink("Shell:Mussels", "Crustacean:Lobster"));
-        pLinks.add(new PLink("Shell:Mussels", "Shell:Whelk"));
-        pLinks.add(new PLink("Crustacean:Crab", "Bird:Gull"));
-        pLinks.add(new PLink("Crustacean:Crab", "Crustacean:Lobster"));
-        pLinks.add(new PLink("Plant:Seaweed", "Shell:Limpets"));
-        pLinks.add(new PLink("Shell:Limpets", "Crustacean:Crab"));
-        pLinks.add(new PLink("Shell:Limpets", "Bird:Gull"));
-        pLinks.add(new PLink("Shell:Limpets", "Shell:Whelk"));
-        pLinks.add(new PLink("Shell:Limpets", "Crustacean:Lobster"));
-        pLinks.add(new PLink("Shell:Whelk", "Bird:Gull"));
-        pLinks.add(new PLink("Shell:Whelk", "Crustacean:Lobster"));
-        wept.addPlinks(pLinks);
-
-        System.out.println("\nWeb Entities:");
-        ArrayList<WebEntity> wes = (ArrayList<WebEntity>) WebEntities.getInstance().getAll();
-        wes.forEach(we->{
-            System.out.print(we.getId() + ". ");
-            we.getPrefixes().forEach(p->{
-                System.out.print(p + " ");
-            });
-            System.out.println();
-            System.out.println("   LRUs:");
-            ArrayList<String> lrus = wept.getPages(we.getId());
-            lrus.forEach(lru->{
-                System.out.println("   - " + lru);
-            });
-            System.out.println("   Links to other web entities:");
-            List<WELink> weLinks = wept.getWelinksOutbound(we.getId());
-            weLinks.forEach(weLink->{
-                System.out.println("   -> " + weLink.targetWebentityid);
-            });
-        });
-        
-        ArrayList<String[]> links = wept._geAllLruLinks_SLOW();
-        System.out.println("\nLRU Links:");
-        links.forEach(link->{
-            System.out.println(link[0] + " -> " + link[1]);
-        });
-    }
-    
-    private static void benchmarkAllWebEntities(WebEntityPageTree wept, boolean display) {
-        ArrayList<WebEntity> wes = (ArrayList<WebEntity>) WebEntities.getInstance().getAll();
-        wes.forEach(we->{
-            try {
-                benchmarkWebEntity(wept, we, display);
-            } catch (IOException ex) {
-                Logger.getLogger(Wetree.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-    }
-    
-    private static void benchmarkRandomWebEntity(WebEntityPageTree wept, boolean display) throws IOException {
-        ArrayList<WebEntity> wes = (ArrayList<WebEntity>) WebEntities.getInstance().getAll();
-        WebEntity we = wes.get(ThreadLocalRandom.current().nextInt(1, wes.size()));
-        benchmarkWebEntity(wept, we, display);
-    }
-    
-    private static void benchmarkWebEntity(WebEntityPageTree wept, WebEntity we, boolean display) throws IOException {
-        if (display) {
-            System.out.print(we.getId() + ". ");
-            we.getPrefixes().forEach(p->{
-                System.out.print(p + " ");
-            });
-            System.out.println();
-        }
-        if (display) System.out.println("   LRUs:");
-        ArrayList<String> lrus = wept.getPages(we.getId());
-        if (display) lrus.forEach(lru->{
-            System.out.println("   - " + lru);
-        });
-        if (display) System.out.println("   Links to other web entities:");
-        List<WELink> weLinks = wept.getWelinksOutbound(we.getId());
-        if (display) weLinks.forEach(weLink->{
-            System.out.println("   -> " + weLink.targetWebentityid);
-        });
-        if (!display) {
-            System.out.println("Web Entity " + we.getId() + ": retrieved " + lrus.size() + " LRUs and links to " + weLinks.size() + " web entities");
-        }
-    }
-
-
     private static void buildFakeCorpus(WebEntityPageTree wept, int webentity_count, int link_count) throws IOException {
         
         // Init
@@ -291,12 +179,13 @@ public class Wetree {
         System.out.print("Creating " + webentity_count + " web entities with pages...");
         while (webentity_count-- > 0) {
             
-            ArrayList<String> prefixes = new ArrayList<String>(ThreadLocalRandom.current().nextInt(1, 8+1));
-            for (int i=0; i<prefixes.size(); i++) {
-                String p = "";
+            int prefix_count = ThreadLocalRandom.current().nextInt(1, 2+1);
+            ArrayList<String> prefixes = new ArrayList<String>(prefix_count);
+            for (int i=0; i<prefix_count; i++) {
+                String p = "s:http|";
                 int prefix_size = ThreadLocalRandom.current().nextInt(2, 5+1);
                 while (prefix_size-- > 0) {
-                    p += stems.get(ThreadLocalRandom.current().nextInt(0, stems.size())) + "|";
+                    p += "h:" + stems.get(ThreadLocalRandom.current().nextInt(0, stems.size())) + "|";
                 }
                 prefixes.add(p);
                 
@@ -306,25 +195,25 @@ public class Wetree {
                     String lru = p;
                     int suffix_size = ThreadLocalRandom.current().nextInt(1, 6+1);
                     while (suffix_size-- > 0) {
-                        lru += stems.get(ThreadLocalRandom.current().nextInt(0, stems.size())) + "|";
+                        lru += "p:" + stems.get(ThreadLocalRandom.current().nextInt(0, stems.size())) + "|";
                     }
                     lrus.add(lru);
                     wept.addPage(lru);
                 }
             }            
             
-            WebEntities.getInstance().create(prefixes);
+            if (ThreadLocalRandom.current().nextInt(0, 3) == 0) WebEntities.getInstance().create(prefixes);
         }
         System.out.println(" done.");
         System.out.println(lrus.size() + " LRUs were created in that process.");
         
         // Create random links
         System.out.print("Creating " + link_count + " random LRU links...");
+        ArrayList<PLink> links = new ArrayList<>();
         while (link_count-- > 0) {
-            String sourcelru = lrus.get(ThreadLocalRandom.current().nextInt(1, lrus.size()));
-            String targetlru = lrus.get(ThreadLocalRandom.current().nextInt(1, lrus.size()));
-            wept.addPlink(sourcelru, targetlru);
+            links.add(new PLink(lrus.get(ThreadLocalRandom.current().nextInt(1, lrus.size())), lrus.get(ThreadLocalRandom.current().nextInt(1, lrus.size()))));
         }
+        wept.addPlinks(links);
         System.out.println(" done.");
     }
     
